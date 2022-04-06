15f624402c77e1f18f9c4162c41b1ec5d6a794d2
HyperSilver69 <blauwemelluk@gmail.com>
2022-04-06 21:11:44 +0200

    New source code added
    New source code added!

---
+++ FirstPersonCamera/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace FirstPersonCamera
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.hypersilver69.gorillatag.firstpersoncamera";
+        public const string Name = "FirstPersonCamera";
+        public const string Version = "1.0.0";
+    }
+}
